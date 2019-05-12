edc761f915953dd4c4f1a1bf501370c6f98ef604
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-05-12 19:48:01 +0530

    Added some required Dev Dependencies & Created the Initial Skeleton

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>Hello World</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
