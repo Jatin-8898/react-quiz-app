f9bcf67454496ee3b15ebf5e88d80e7293dbf19f
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-05-15 17:15:51 +0530

    Added the Questions & Answers

---
+++ src/components/questions_data/data.js
@@ -0,0 +1,73 @@
+export default [
+    {
+        question: '. We can go for keys when there is possibility that our user could change the data?',
+        answers: [
+            'Keys',
+            'ref',
+            'both',
+            'none of above'
+        ],
+        correct: 1
+    },
+
+    {
+        question: 'JSX is typesafe?',
+        answers: [
+            'True',
+            'False',
+            'None of the Above',
+            'Cannot Be Determined'
+        ],
+        correct: 1
+    },    
+    {
+        question: 'React merges the object you provide into the current state using which of the following',
+        answers: [
+            'setState()',
+            'State()', 
+            'getState()', 
+            'None of the Above'
+        ],    
+        correct: 1
+    },    
+    {
+        question: 'Arbitrary inputs of components are called?',
+        answers: [
+            'Keys',
+            'Props', 
+            'Elements', 
+            'Ref'
+        ],    
+        correct: 2
+    },
+    {
+        question: '_____ can be done while more than one element needs to be returned from a component?',
+        answers: [
+            'Abstraction',
+            'Packing', 
+            'Insulation', 
+            'Wrapping'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Which of the following needs to be updated to achieve dynamic UI updates?',
+        answers: [
+            'State',
+            'Props', 
+            'Components', 
+            'None of the Above'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Lifecycle methods are mainly used _____',
+        answers: [
+            'To keep track of event history',
+            'To enhance components', 
+            'Free up resources', 
+            'None of the Above'
+        ],    
+        correct: 3
+    }
+]
