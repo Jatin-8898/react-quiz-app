e6aa9df1e798a306c0173016ea26e05caf61a132
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-05-17 23:12:53 +0530

    Chcking the Correct Ans & updating the states ,but one issue the right,wrong doesn't get removed

---
+++ src/components/core/Answers.jsx
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+
+class Answers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAnswered: false,
+            classNames: ['', '', '', '']
+        }
+        
+        this.checkAnswer = this.checkAnswer.bind(this);
+    }
+    
+    checkAnswer(e) {
+        let { isAnswered } = this.props;
+        
+        if(!isAnswered) {
+            let elem = e.currentTarget;
+            let { correct, increaseScore } = this.props;
+            let answer = Number(elem.dataset.id);
+            let updatedClassNames = this.state.classNames;
+
+            if(answer === correct){
+                updatedClassNames[answer-1] = 'right';
+                this.setState({
+                    classNames: ['', '', '', '']
+                })
+                increaseScore();
+            }
+            else {
+                updatedClassNames[answer-1] = 'wrong';
+                this.setState({
+                    classNames: ['', '', '', '']
+                })
+            }
+            
+            this.setState({
+                classNames: updatedClassNames
+            })
+
+            this.props.showButton();
+        }
+    }
+    
+    /* shouldComponentUpdate() {
+        this.setState({
+            classNames: ['', '', '', '']
+        })
+        return false
+      }  */
+
+    render() {
+        let { answers } = this.props;
+        let { classNames } = this.state;
+        
+        let transition = {
+            transitionName: "example",
+            transitionEnterTimeout: 500,
+            transitionLeaveTimeout: 300
+        }
+        
+        return (
+            <div id="answers">
+                <ul>
+                    <li onClick={this.checkAnswer} 
+                        className={classNames[0]} data-id="1">
+                    <span>A</span> 
+                    <p>{answers[0]}</p></li>
+
+                    <li onClick={this.checkAnswer} 
+                        className={classNames[1]} data-id="2">
+                    <span>B</span> 
+                    <p>{answers[1]}</p></li>
+
+                    <li onClick={this.checkAnswer} 
+                        className={classNames[2]} data-id="3">
+                    <span>C</span> 
+                    <p>{answers[2]}</p></li>
+
+                    <li onClick={this.checkAnswer} 
+                        className={classNames[3]} data-id="4">
+                    <span>D</span> 
+                    <p>{answers[3]}</p></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Answers
