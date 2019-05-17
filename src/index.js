58b9ec79793c1f007f36ae03eca4e5b4dc749872
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-05-17 23:14:34 +0530

    Without jsx throws module not found so solved by adding the extension name :tada:

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './resources/styles.css';
 
-import Header from './components/header_footer/Header';
-import Main from './components/core/Main';
-import Footer from './components/header_footer/Footer';
+import Header from './components/header_footer/Header.jsx';
+import Main from './components/core/Main.jsx';
+import Footer from './components/header_footer/Footer.jsx';
 
 class App extends React.Component {
     render() {
