a215885dcce9d1fbcaaf6e0726c85cce5216b6b3
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-05-19 13:57:10 +0530

    Modified to generate the build

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin =  require('html-webpack-plugin');
 module.exports = {
     entry : './src/index.js',
     output : {
-        path : path.resolve(__dirname , 'dist'),
+        path : path.resolve(__dirname , 'build'),
         filename: 'index_bundle.js'
     },
    
